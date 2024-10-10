2780f8f632995b2f7aa539574c0c47f68329306b
Foreji14 <rafaelandries@yahoo.ro>
2024-10-10 14:09:03 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import UserList from "./components/UserList";
-import Header from "./components/Header";
+import UserList from "./UserList";
+import Header from "./Header";
 export default function App() {
   const [data, updateData] = useState([])
-  useEffect(()=>{
+  useEffect(() => {
     // API call to get user list
     axios
       .get("https://randomuser.me/api/?results=50")
       .then(response => {
         const data = response.data.results;
         updateData(data);
+        console.log(data);
       })
       .catch(error => {
         console.log(error);
       });
-  },[]) 
+  }, [])
   return (
-      <div className="App">
-        <Header title={"UserList"} />
-        <div className="row d-flex justify-content-center">
-          {data.map((item, index) => (
-            <UserList key={index} item={item} />
-          ))}
-        </div>
+    <div className="App">
+      <Header title={"UserList"} />
+      <div className="row d-flex justify-content-center">
+        {data.map((item, index) => (
+          <UserList key={index} item={item} />
+        ))}
       </div>
-    );
+    </div>
+  );
 }
